1b3fb8ef88a13689c6d14952233684ebf93a5dc0
Lawrence Godfrey <lawrence.godfrey@kazang.com>
2023-03-18 12:42:37 +0200

    Add script for deploying slash commands

---
+++ deploy-commands.js
@@ -0,0 +1,27 @@
+import { REST, Routes } from 'discord.js';
+import dotenv from 'dotenv';
+import joinCommand from './commands/join.js';
+
+dotenv.config();
+
+
+// Construct and prepare an instance of the REST module
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+// and deploy your commands!
+(async () => {
+    try {
+        console.log(`Started refreshing application (/) commands.`);
+
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIEND_ID, process.env.GUILD_ID),
+            { body: [joinCommand.data.toJSON()] },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error);
+    }
+})();
